3ea7f9195313efbc9501d3b5b820ccdbdd1c528f
Vasuki-T <iamvasukithangaraj@gmail.com>
2026-04-20 18:38:09 +0530

    added home page with header & footer

---
+++ Components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return <footer className="fixed inset-x-0 bottom-0 flex flex-col items-center bg-neutral-900 text-center text-white">
+        <div className="w-full p-4 text-center bg-black">
+            <p>@ 2026 Blog-platform</p>
+        </div>
+    </footer>
+}
